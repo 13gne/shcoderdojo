3245cadb35f3dc1c093ea06e51620346056cc560
Sid Jayanna <sjayanna@gmail.com>
2016-05-03 15:05:01 -0400

    prevent any user to access admin dashboard

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,11 +7,21 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_filter :authenticate_admin
+    before_action :authenticate_user!
+
+    private
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      not_found unless current_user && current_user.admin?
     end
 
+    def not_found
+      begin
+        raise ActionController::RoutingError, 'Not Found'
+      rescue
+        redirect_to root_path, alert: "You do not have access to that resource!"
+      end
+    end
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
     # def records_per_page
