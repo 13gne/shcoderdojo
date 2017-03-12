223fb2fda2a745cfba1d928f657e5590e6281512
Jeff Juszczak <jeff@moottoast.com>
2017-03-12 12:52:42 -0400

    Change to login by email and update permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     courses_path
   end
 
-  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:email, :password, :password_confirmation, :parent_or_student)
+    end
+  end
 end
