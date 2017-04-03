591e24c15892b5742daf4ca9026c5a3d94f809cf
Jeff Juszczak <jeff@moottoast.com>
2017-04-02 22:02:28 -0400

    Add some more sorting and searching for admin users index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ module Admin
     def index
       @q = User.search(params[:q])
       @q.sorts = 'name asc' if @q.sorts.empty?
-      @users = @q.result
+      @users = @q.result.includes(:students)
     end
   end
 end
