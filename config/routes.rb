651eb74cc0a2413fda315f3ec590d66d4b6163cd
Jeff Juszczak <jeff@moottoast.com>
2017-04-03 17:04:14 -0400

    WIP - user admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
     resources :attendances
+    resources :courses, only: [:new, :create, :edit, :update, :destroy]
     root to: 'users#index'
   end
   resources :course_registrations
   resources :students
   resources :courses
   resources :sessions, as: :course_sessions
-
   root to: 'visitors#index'
   devise_for :users
   resources :users
