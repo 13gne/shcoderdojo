2b096eb546dd0db94496417c75a28357a0ff40a3
Jeff Juszczak <jeff@moottoast.com>
2017-04-10 10:22:33 -0400

    Fixes #23

--- app/controllers/admin/course_registrations_controller.rb
+++ app/controllers/admin/course_registrations_controller.rb
@@ -4,9 +4,7 @@ module Admin
       @course = Course.find(params[:course_id])
       @sessions = Session.where(course_id: @course.id).order(:session_date)
       @q = CourseRegistration.where(course: @course).ransack(params[:q])
-      Rails.logger.debug "SORTS before: #{@q.sorts}"
       @q.sorts = 'students.name' if @q.sorts.empty?
-      Rails.logger.debug "SORTS: #{@q.sorts}"
       @course_registrations = @q.result.includes(:student)
     end
   end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,9 +2,7 @@ module Admin
   class UsersController < AdminBaseController
     def index
       @q = User.search(params[:q])
-      Rails.logger.debug "SORTS before: #{@q.sorts}"
       @q.sorts = 'name asc' if @q.sorts.empty?
-      Rails.logger.debug "SORTS: #{@q.sorts}"
       @users = @q.result.includes(:students)
     end
 
