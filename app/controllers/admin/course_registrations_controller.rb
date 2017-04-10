36e74036ac14f65de88db48c1d76c200373568f1
Jeff Juszczak <jeff@moottoast.com>
2017-04-10 10:16:11 -0400

    Sort and search student registrations

--- app/controllers/admin/course_registrations_controller.rb
+++ app/controllers/admin/course_registrations_controller.rb
@@ -3,7 +3,11 @@ module Admin
     def index
       @course = Course.find(params[:course_id])
       @sessions = Session.where(course_id: @course.id).order(:session_date)
-      @course_registrations = CourseRegistration.where(course: @course).includes(:student).order('students.name')
+      @q = CourseRegistration.where(course: @course).ransack(params[:q])
+      Rails.logger.debug "SORTS before: #{@q.sorts}"
+      @q.sorts = 'students.name' if @q.sorts.empty?
+      Rails.logger.debug "SORTS: #{@q.sorts}"
+      @course_registrations = @q.result.includes(:student)
     end
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,9 @@ module Admin
   class UsersController < AdminBaseController
     def index
       @q = User.search(params[:q])
+      Rails.logger.debug "SORTS before: #{@q.sorts}"
       @q.sorts = 'name asc' if @q.sorts.empty?
+      Rails.logger.debug "SORTS: #{@q.sorts}"
       @users = @q.result.includes(:students)
     end
 
