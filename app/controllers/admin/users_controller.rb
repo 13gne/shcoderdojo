59630a83e3d00af704103aada321dff57b46e921
Jeff Juszczak <jeff@moottoast.com>
2017-10-02 18:45:18 -0400

    Complete admin's ability to edit a user's details

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 module Admin
   class UsersController < AdminBaseController
+    before_action :set_user, only: [:edit, :update]
     def index
       @q = User.search(params[:q])
       @q.sorts = 'name asc' if @q.sorts.empty?
@@ -7,11 +8,30 @@ module Admin
     end
 
     def edit
-      @user = User.find(params[:id])
     end
 
     def show
       @user = User.includes(:students).find(params[:id])
+    end
+
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to admin_user_path(@user) }
+        else
+          format.html { render :edit }
+        end
+      end
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :mobile_number, :role)
     end
   end
 end
