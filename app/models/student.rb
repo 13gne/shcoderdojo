17a4c2f3eb1eab2d90e4e5324bef0d8a8b27a638
Jeff Juszczak <jeff@moottoast.com>
2018-05-05 15:19:56 -0400

    Update ERD

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,7 +15,7 @@ class Student < ActiveRecord::Base
   has_many :course_registrations, dependent: :destroy
   has_many :attendances, dependent: :destroy
   has_many :student_achievements, dependent: :destroy
-  has_many :courses, :through => :attendances
+  has_many :courses, :through => :course_registrations
 
   validates :user_id, presence: true
   validates :name, presence: true
