2fada039f6952d66bc4de0971bde01acdcaa30d0
Jeff Juszczak <jeff@moottoast.com>
2018-05-28 11:23:34 -0400

    Belt specs

--- spec/models/belt_spec.rb
+++ spec/models/belt_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Belt, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Belt do
+  context 'database fields' do
+    it { should have_db_column(:name) }
+    it { should have_db_column(:sequence) }
+  end
+
+  context 'associations' do
+    it { should have_many(:achievements) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:sequence) }
+  end
 end
