ae3471b78968d82745b71375f818678e9fbbb310
Jeff Juszczak <jeff@moottoast.com>
2018-05-28 16:05:38 -0400

    Course registrations spec

--- spec/models/course_registration_spec.rb
+++ spec/models/course_registration_spec.rb
@@ -1,17 +1,24 @@
-# == Schema Information
-#
-# Table name: course_registrations
-#
-#  id         :integer          not null, primary key
-#  user_id    :integer
-#  student_id :integer
-#  course_id  :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 require 'rails_helper'
 
-RSpec.describe CourseRegistration, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe CourseRegistration do
+  context 'database fields' do
+    it { should have_db_column(:user_id) }
+    it { should have_db_column(:student_id) }
+    it { should have_db_column(:course_id) }
+  end
+
+  context 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:student) }
+    it { should belong_to(:course) }
+  end
+
+  context 'validations' do
+    context 'achieved minimum level' do
+      before { allow(subject).to receive(:minimum_level_achieved).and_return(true) }
+      it { should validate_presence_of(:user_id) }
+      it { should validate_presence_of(:student_id) }
+      it { should validate_presence_of(:course_id) }
+    end
+  end
 end
